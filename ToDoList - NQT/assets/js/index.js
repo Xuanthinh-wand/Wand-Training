b965ed2ef5903d580eda62f60a4a20a6e0101533
quoctinh760 <88821431+quoctinh760@users.noreply.github.com>
2021-09-07 00:44:07 +0700

    Commit Bài tập To Do List

---
+++ ToDoList - NQT/assets/js/index.js
@@ -0,0 +1,50 @@
+createList(arrayElement);
+// Hiển thị danh sách To do List
+function createList(arrayElem) {
+    for (var i = 0;i < arrayElem.length;i++) {
+        arrayElem[i].id = i;
+    }
+    for (let element of arrayElem) {
+        if (element.title !== '') {
+            var li = document.createElement("li");
+            li.id = element.id;
+            li.onclick = function () {
+                element.isCompleted = !element.isCompleted;
+                document.getElementById("myUL").innerHTML = '';
+                createList(arrayElem);
+            }
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            span.onclick = function () {
+                arrayElem.splice(element.id, 1);
+                console.log(element.id);
+                document.getElementById("myUL").innerHTML = '';
+                createList(arrayElem);
+            };
+            li.appendChild(span);
+            if (element.isCompleted == true) {
+                li.classList.add("checked");
+            }
+            var inputValue = element.title;
+            var t = document.createTextNode(inputValue);
+            li.appendChild(t);
+            document.getElementById("myUL").appendChild(li);
+        }
+
+    }
+}
+// Thêm mới Element
+var newElement = () => {
+    var inputValue = document.getElementById("myInput").value;
+    if (inputValue === '') {
+        alert("Nội dung trống!");
+    } else {
+        var newItem = { id: arrayElement.length, title: inputValue, isCompleted: false }
+        arrayElement.push(newItem);
+    }
+    document.getElementById("myInput").value = "";
+    document.getElementById("myUL").innerHTML = '';
+    createList(arrayElement);
+}
