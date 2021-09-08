e63dde25f66de7377ca089aee0ce930fdc4e09c2
quoctinh760 <88821431+quoctinh760@users.noreply.github.com>
2021-09-08 15:42:46 +0700

    Cm nút bắt đầu làm bài

--- Quiz-JS/assets/js/index.js
+++ Quiz-JS/assets/js/index.js
@@ -20,40 +20,16 @@ var arrayQuesition = [
         answer: "while (i <= 10; i++)"
     }
 ];
-var arrayAnswer = [{}];
-// Set the date we're counting down to
-var countDownDate = Date.now() + 6000;
+var x;
+document.getElementById("sum-quesition").appendChild(document.createTextNode("Tổng số câu hỏi là: " + arrayQuesition.length));
+document.getElementById("btn-nopbai").style.display = "none";
+function start() {
+    document.getElementById("btn-nopbai").style.display = "block";
+    document.getElementById("btn-start").style.display = "none";
+    createList(arrayQuesition);
+    batdau();
+}
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    // var timenow = Date.now();
-    console.log(distance);
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "Time: " + minutes + " m " + seconds + "s ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Hết giờ";
-        var link = document.getElementById("btn-nopbai");
-        link.click();
-    }
-}, 1000);
-
-createList(arrayQuesition);
-// check();
 function createList(arrayCH) {
     console.log(arrayCH);
     for (let cauhoi of arrayCH) {
@@ -80,6 +56,35 @@ function createList(arrayCH) {
         }
     }
 }
+function batdau() {
+    var countDownDate = Date.now() + 12000;
+    x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // var timenow = Date.now();
+        console.log(distance);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("time").innerHTML = "Time: " + minutes + " m " + seconds + "s ";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("time").innerHTML = "Hết giờ";
+            var link = document.getElementById("btn-nopbai");
+            link.click();
+        }
+    }, 1000);
+}
 function getCheckedValue(radioName) {
     var radios = document.getElementsByName(radioName); // Get radio group by-name
     for (let rd of radios)
@@ -92,7 +97,7 @@ function check() {
         console.log("cauhoi" + ch.id);
         if (getCheckedValue("cauhoi" + ch.id) == ch.answer) score++;
     }
-    var diem = "Số điểm của bạn là " + score / arrayQuesition.length * 10;
+    var diem = "Số điểm của bạn là " + score / arrayQuesition.length * 10 + "/10";
     var span = document.createElement("span");
     span.appendChild(document.createTextNode(diem));
     var kq = "Bạn trả lời đúng " + score + "/" + arrayQuesition.length + " câu hỏi";
