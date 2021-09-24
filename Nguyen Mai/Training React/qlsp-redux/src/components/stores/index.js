503613313e3ba1db7e011f62bd5424361206fc09
Nguyenmai98 <maivinhuni98@gmail.com>
2021-09-24 16:04:01 +0700

    update

---
+++ Nguyen Mai/Training React/qlsp-redux/src/components/stores/index.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import todoProduct from '../reducers/index'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+    key: 'root',
+    storage: storage
+   };
+const ShopApp = combineReducers({
+    _todoProduct:todoProduct
+});
+const pReducer = persistReducer(persistConfig, ShopApp);
+const store =  createStore(pReducer,applyMiddleware(thunkMiddleware));
+
+export default store;
+export const persistor = persistStore(store);
