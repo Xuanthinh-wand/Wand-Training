acab227b3bf59dfd9e3d697a50fffa7293182ef6
TRUONG 99 <ledoantruong1999@gmail.com>
2022-06-22 00:40:46 +0700

    Update

--- LeTruong/SliderImage/js/main.js
+++ LeTruong/SliderImage/js/main.js
@@ -4,15 +4,23 @@ const slider = document.querySelector('.slider');
 const sliderItems = document.querySelectorAll('.slider-item');
 const sliderMain = document.querySelector('.slider-main');
 const sliderDot = document.querySelector('.slider-dots');
+const sliderIndex = document.querySelector('.sum-index')
+const currentItemIndex = document.querySelector('.current-index')
 const sliderItemWidth = sliderItems[0].offsetWidth;
 const sliderItemLength = sliderItems.length;
 let index = 0;
 let positionItemX = 0;
-
+var setAutoPlaySlider;
+function setCurrentIndex() {
+    let currentIndex = index + 1;
+    currentItemIndex.innerText = currentIndex
+}
+setCurrentIndex()
 const config = {
     autoplay: true,
-    autoplaySpeed: 10000,
+    autoplaySpeed: 5000,
 }
+sliderIndex.innerText = sliderItemLength
 
 for (var i = 0; i < sliderItemLength; i++) { 
     sliderDot.innerHTML += "<li class=\"slider-dots-item\"><span class=\"slider-dots-btn\"></span></li>"
@@ -21,68 +29,80 @@ for (var i = 0; i < sliderItemLength; i++) {
 const sliderDotItems = document.querySelectorAll('.slider-dots-btn');
 sliderDotItems[index].classList.add('active')
 
+function autoPlaySlider() {
+     handleClickSlider(1)
+}
+
+// auto play anh
+if (config.autoplay) {
+    setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
+}
 
 //start click next prev de slider anh
 btnNext.addEventListener('click', function () {
-    sliderNext()
+    handleClickSlider(1)
 })
 
 btnPrev.addEventListener('click', function () {
-    sliderPrev()
+    handleClickSlider(-1)
 })
 
-// next anh
-function sliderNext() {
+
+function handleClickSlider(num) {
+    if (setAutoPlaySlider) {
+        clearInterval(setAutoPlaySlider)
+    }
     sliderDotItems[index].classList.remove('active')
-    if (index >= sliderItemLength - 1) {
+    if (num === 1) {
+         if (index >= sliderItemLength - 1) {
             index = 0
             positionItemX = 0
             sliderMain.style = `transform: translateX(${positionItemX}px)`
             sliderDotItems[index].classList.add('active')
+            setCurrentIndex()
+            if (config.autoplay) {
+            setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
+    }
         return;
     }
     positionItemX -= sliderItemWidth
-    sliderMain.style = `transform: translateX(${positionItemX}px)`
     index++;
-    sliderDotItems[index].classList.add('active')
-}
+    }
 
-//prev anh
-function sliderPrev() {
-    sliderDotItems[index].classList.remove('active')
-    if (index === 0) {
+    if (num === -1) {
+          if (index === 0) {
             index = sliderItemLength - 1
             positionItemX = -1 * sliderItemWidth * (sliderItemLength - 1)
             sliderMain.style = `transform: translateX(${positionItemX}px)`
             sliderDotItems[index].classList.add('active')
-        return
+            setCurrentIndex()
+            if (config.autoplay) {
+            setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
     }
-    positionItemX += sliderItemWidth
+        return
+         }
+        positionItemX += sliderItemWidth
+        index--;
+    }
     sliderMain.style = `transform: translateX(${positionItemX}px)`
-    index--;
     sliderDotItems[index].classList.add('active')
+    setCurrentIndex()
+    if (config.autoplay) {
+        setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
+    }
 }
-//end click slider anh
-
 
 //click dot slider
 sliderDotItems.forEach(function (item, itemIndex) {
     item.addEventListener('click', function () {
         sliderDotItems[index].classList.remove('active')
         positionItemX = -1 * sliderItemWidth * itemIndex
-        sliderMain.style = `transform: translateX(${positionItemX}px)`
         index = itemIndex
+        sliderMain.style = `transform: translateX(${positionItemX}px)`
         sliderDotItems[index].classList.add('active')
-
+        setCurrentIndex()
     });
 })
- 
-// auto play anh
-if (config.autoplay) {
-    setInterval(
-        function autoPlaySlider() {
-            sliderNext()
-        },config.autoplaySpeed);
-}
+
 
 
