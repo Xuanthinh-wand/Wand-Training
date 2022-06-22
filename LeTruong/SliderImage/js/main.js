85de020e94c3cb44d9c5d1adc09834187cf290aa
TRUONG 99 <ledoantruong1999@gmail.com>
2022-06-23 00:06:51 +0700

    Update config slider

--- LeTruong/SliderImage/js/main.js
+++ LeTruong/SliderImage/js/main.js
@@ -1,108 +1,160 @@
-const btnPrev = document.querySelector('.btn-prev');
-const btnNext = document.querySelector('.btn-next');
-const sliderItems = document.querySelectorAll('.slider-item');
-const sliderMain = document.querySelector('.slider-main');
-const sliderDot = document.querySelector('.slider-dots');
-const sliderIndex = document.querySelector('.sum-index');
-const currentItemIndex = document.querySelector('.current-index');
-const sliderItemWidth = sliderItems[0].offsetWidth;
-const sliderItemLength = sliderItems.length;
-let index = 0;
-let positionItemX = 0;
-var setAutoPlaySlider;
-function setCurrentIndex() {
-    let currentIndex = index + 1;
-    currentItemIndex.innerText = currentIndex;
-}
-setCurrentIndex();
-const config = {
-    autoplay: true,
-    autoplaySpeed: 5000,
-};
-sliderIndex.innerText = sliderItemLength;
+function slider({
+    className: yourclass,
+    dots: dots = true,
+    autoPlay: autoPlay = true,
+    autoPlaySpeed: autoPlaySpeed = 5000,
+    arrows: arrows = true,
+    indexCurrent: indexCurrent = true,
+}) {
+    const sliderMain = document.querySelector(yourclass);
+    const elChild = document.querySelectorAll(`${yourclass}  > *`);
 
-for (var i = 0; i < sliderItemLength; i++) {
-    sliderDot.innerHTML += '<li class="slider-dots-item"><span class="slider-dots-btn"></span></li>';
-}
+    const sliderItemWidth = elChild[0].offsetWidth;
+    const sliderItemLength = elChild.length;
+    let index = 0;
+    let positionItemX = 0;
+    var setAutoPlaySlider;
+    // gộp tất cả các element slick vào chung 1 element cha
+    sliderMain.innerHTML = "<div class='slider-list'></div>";
+    let sliderList = document.querySelector('.slider-list');
+    elChild.forEach((item) => {
+        sliderList.appendChild(item);
+    });
 
-const sliderDotItems = document.querySelectorAll('.slider-dots-btn');
-sliderDotItems[index].classList.add('active');
+    // thêm các chỉ số chại slider
+    if (indexCurrent) {
+        let elIndex = `<div class=\"slider-index\"><span class=\"current-index\"></span> \/ <span class=\"sum-index\">${sliderItemLength}</span></div>`;
+        sliderMain.innerHTML += elIndex;
 
-function autoPlaySlider() {
-    handleClickSlider(1);
-}
-
-// auto play anh
-if (config.autoplay) {
-    setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
-}
-
-//start click next prev de slider anh
-btnNext.addEventListener('click', function () {
-    handleClickSlider(1);
-});
-
-btnPrev.addEventListener('click', function () {
-    handleClickSlider(-1);
-});
-
-function handleClickSlider(num) {
-    if (setAutoPlaySlider) {
-        clearInterval(setAutoPlaySlider);
+        function setCurrentIndex() {
+            let eleCurrentIndex = document.querySelector('.current-index');
+            let currentIndex = index + 1;
+            eleCurrentIndex.innerText = currentIndex;
+        }
+        setCurrentIndex();
     }
-    sliderDotItems[index].classList.remove('active');
-    if (num === 1) {
-        if (index >= sliderItemLength - 1) {
-            index = 0;
-            positionItemX = 0;
-            sliderMain.style = `transform: translateX(${positionItemX}px)`;
+
+    // thêm các button next prev
+    if (arrows && elChild.length > 1) {
+        sliderMain.innerHTML +=
+            '<button class="btn btn-prev"> <i class="fa-solid fa-angle-left"></i></button><button button class="btn btn-next" > <i class="fa-solid fa-angle-right"></i></button > ';
+    }
+
+    // thêm dot cho slider
+    if (dots && sliderItemLength > 1) {
+        sliderMain.innerHTML += '<ul class="slider-dots"></ul>';
+        const sliderDot = document.querySelector('.slider-dots');
+        for (var i = 0; i < sliderItemLength; i++) {
+            sliderDot.innerHTML += '<li class="slider-dots-item"><span class="slider-dots-btn"></span></li>';
+        }
+
+        const sliderDotItems = document.querySelectorAll('.slider-dots-btn');
+        addActiveDot();
+
+        //click dot slider
+        sliderDotItems.forEach(function (item, itemIndex) {
+            sliderList = document.querySelector('.slider-list'); // phải gán lại mới chạy ?
+            item.addEventListener('click', function () {
+                if (setAutoPlaySlider) {
+                    clearInterval(setAutoPlaySlider);
+                }
+                removeActiveDot();
+                positionItemX = -1 * sliderItemWidth * itemIndex;
+                index = itemIndex;
+                sliderList.style = `transform: translateX(${positionItemX}px)`;
+                addActiveDot();
+                setCurrentIndex();
+                if (autoPlay) {
+                    setAutoPlaySlider = setInterval(autoPlaySlider, autoPlaySpeed);
+                }
+            });
+        });
+
+        //xóa active của dot
+        function removeActiveDot() {
+            sliderDotItems[index].classList.remove('active');
+        }
+        //Thêm active của dot
+        function addActiveDot() {
             sliderDotItems[index].classList.add('active');
-            setCurrentIndex();
-            if (config.autoplay) {
-                setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
-            }
-            return;
         }
     }
 
-    if (num === -1) {
-        if (index === 0) {
-            index = sliderItemLength - 1;
-            positionItemX = -1 * sliderItemWidth * (sliderItemLength - 1);
-            sliderMain.style = `transform: translateX(${positionItemX}px)`;
-            sliderDotItems[index].classList.add('active');
-            setCurrentIndex();
-            if (config.autoplay) {
-                setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
-            }
-            return;
-        }
+    function autoPlaySlider() {
+        handleClickSlider(1);
     }
 
-    positionItemX -= num * sliderItemWidth;
-    index = index + num;
-    sliderMain.style = `transform: translateX(${positionItemX}px)`;
-    sliderDotItems[index].classList.add('active');
-    setCurrentIndex();
-    if (config.autoplay) {
-        setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
+    // auto play anh
+    if (autoPlay && sliderItemLength > 1) {
+        setAutoPlaySlider = setInterval(autoPlaySlider, autoPlaySpeed);
     }
-}
 
-//click dot slider
-sliderDotItems.forEach(function (item, itemIndex) {
-    item.addEventListener('click', function () {
+    // start click next prev de slider anh
+    if (arrows) {
+        const btnPrev = document.querySelector('.btn-prev');
+        const btnNext = document.querySelector('.btn-next');
+        btnNext.addEventListener('click', function () {
+            handleClickSlider(1);
+        });
+
+        btnPrev.addEventListener('click', function () {
+            handleClickSlider(-1);
+        });
+    }
+
+    function handleClickSlider(num) {
+        sliderList = document.querySelector('.slider-list');
+        removeActiveDot();
         if (setAutoPlaySlider) {
             clearInterval(setAutoPlaySlider);
         }
-        sliderDotItems[index].classList.remove('active');
-        positionItemX = -1 * sliderItemWidth * itemIndex;
-        index = itemIndex;
-        sliderMain.style = `transform: translateX(${positionItemX}px)`;
-        sliderDotItems[index].classList.add('active');
-        setCurrentIndex();
-        if (config.autoplay) {
-            setAutoPlaySlider = setInterval(autoPlaySlider, config.autoplaySpeed);
+        if (num === 1) {
+            if (index >= sliderItemLength - 1) {
+                index = 0;
+                positionItemX = 0;
+                sliderList.style = `transform: translateX(${positionItemX}px)`;
+                if (dots && sliderItemLength > 1) {
+                    addActiveDot();
+                }
+                if (indexCurrent && sliderItemLength > 1) {
+                    setCurrentIndex();
+                }
+                if (autoPlay) {
+                    setAutoPlaySlider = setInterval(autoPlaySlider, autoPlaySpeed);
+                }
+                return;
+            }
         }
-    });
-});
+
+        if (num === -1) {
+            if (index === 0) {
+                index = sliderItemLength - 1;
+                positionItemX = -1 * sliderItemWidth * (sliderItemLength - 1);
+                sliderList.style = `transform: translateX(${positionItemX}px)`;
+                if (dots && sliderItemLength > 1) {
+                    addActiveDot();
+                }
+                if (indexCurrent && sliderItemLength > 1) {
+                    setCurrentIndex();
+                }
+                if (autoPlay) {
+                    setAutoPlaySlider = setInterval(autoPlaySlider, autoPlaySpeed);
+                }
+                return;
+            }
+        }
+
+        positionItemX -= num * sliderItemWidth;
+        index = index + num;
+        sliderList.style = `transform: translateX(${positionItemX}px)`;
+        if (dots && sliderItemLength > 1) {
+            addActiveDot();
+        }
+        if (indexCurrent && sliderItemLength > 1) {
+            setCurrentIndex();
+        }
+        if (autoPlay) {
+            setAutoPlaySlider = setInterval(autoPlaySlider, autoPlaySpeed);
+        }
+    }
+}
