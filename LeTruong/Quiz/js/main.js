c6934d8b5a5d8dc3819161662474b002d7ae75c0
PC-25837\Admin <ledoantruong1999@gmail.com>
2022-06-25 17:33:25 +0700

    Update

--- LeTruong/Quiz/js/main.js
+++ LeTruong/Quiz/js/main.js
@@ -8,41 +8,42 @@ const eleListQuestion = document.querySelector('.questions-list');
 const eleQuestionTabs = document.querySelector('.questions-tabs');
 let totalQuestions = 5;
 let currentIndex = 0;
+let arrIndex = [];
 
 // Lấy random ra 5 câu hỏi
 getRandomQuestion(totalQuestions);
 function getRandomQuestion(number) {
-    var arrIndex = [];
     while (arrIndex.length < number) {
-        var randomIndex = Math.floor(Math.random() * questions.length);
+        var randomIndex = Math.ceil(Math.random() * questions.length);
         if (arrIndex.indexOf(randomIndex) === -1) {
             arrIndex.push(randomIndex);
         }
     }
-
     //In ra list câu hỏi
-    arrIndex.forEach((item, index) => {
+    arrIndex.forEach((questionId, index) => {
+        const question = questions.find((question) => question.id == questionId);
         eleListQuestion.innerHTML += `<div class=\"questions-item\"><strong>Câu ${index + 1}: </strong>${
-            questions[item].title
+            question.title
         }</div>`;
-        let eleQuestionItem = document.querySelectorAll('.questions-item');
+
+        let eleQuestionItems = document.querySelectorAll('.questions-item');
+        eleQuestionItems[index].innerHTML += '<div class="answers-list"></div>';
+        const eleAnswerList = document.querySelectorAll('.answers-list');
         let listAnswers = '';
-        listAnswers = answers.filter((answer) => answer.questionId == questions[item].id);
+        listAnswers = answers.filter((answer) => answer.questionId == questionId);
 
         //In ra các tab để click đến từng câu hỏi
         eleQuestionTabs.innerHTML += `<div class=\"questions-tabs-item\">${index + 1}</div>`;
         //In ra list đáp án theo từng câu hỏi
         listAnswers.forEach((answer) => {
-            eleQuestionItem[
+            eleAnswerList[
                 index
-            ].innerHTML += `<div class=\"questions-answer\"><label><input type=\"radio\" name=\"quiz-${index + 1}\"> ${
-                answer.name
-            }</label></div>`;
+            ].innerHTML += `<div class=\"questions-answer\"><label><input type=\"radio\" class=\"checked-${
+                index + 1
+            }\" data-id=\"${answer.id}\" name=\"quiz-${index + 1}\"> ${answer.name}</label></div>`;
         });
     });
 }
-
-eleQuestions.innerHTML += '<button type="submit" class="btn-submit">Nộp bài </button>';
 
 // Thêm btn prev next question
 eleQuestions.innerHTML +=
@@ -105,9 +106,73 @@ eleTabItems.forEach((tab, index) => {
     });
 });
 
+let initBtnSubmit = document.createElement('button');
+initBtnSubmit.classList.add('btn-submit');
+initBtnSubmit.innerText = 'Nộp bài';
+eleQuestions.appendChild(initBtnSubmit);
 const btnSubmit = document.querySelector('.btn-submit');
-function submitQuestion() {
-    btnSubmit.addEventListener('click', (question, index) => {
-        console.log('submit');
+
+const eleAnswerList = document.querySelectorAll('.answers-list');
+eleAnswerList.forEach((answer, index) => {
+    const childs = answer.childNodes;
+    childs.forEach((child) => {
+        child.addEventListener('click', function () {
+            this.classList.add('checked');
+            console.log(this);
+        });
     });
-}
+});
+
+// eleRadios.forEach((item, index) => {
+//     const tabActive = eleTabItems[index];
+//     item.addEventListener('click', function () {
+//         console.log(index);
+//         this.classList.add('cheked');
+//         tabActive.style = 'color: green; border-color: green;';
+//     });
+// });
+
+btnSubmit.addEventListener('click', () => {
+    let indexRequired = '';
+    let arrAnswersResult = [];
+    let answerSubmit = {};
+    arrIndex.forEach((questionId, index) => {
+        let answerId = 0;
+
+        // let statusCheckeds = document.querySelectorAll('.questions-item input[type=radio]:checked');
+        // console.log(statusCheckeds);
+        // if (!statusCheckeds[index]) {
+        //     indexRequired += index + ',';
+        //     return;
+        // }
+        let eleCheckeds = document.querySelectorAll(`.checked-${index + 1}`);
+        eleCheckeds.forEach((item, index) => {
+            if (eleCheckeds[index].checked) {
+                answerId = eleCheckeds[index].dataset.id;
+            }
+        });
+        let statusAnswer = false;
+        let getStatusAnswersResult = answers.find((answer) => answer.id == answerId);
+        if (getStatusAnswersResult) {
+            statusAnswer = getStatusAnswersResult.status;
+        }
+        let answerTrue = answers.find((answer) => answer.questionId == questionId && answer.status == true);
+        answerSubmit = {
+            userId: 1,
+            questionId: questionId,
+            answerId: answerId,
+            answerTrueId: answerTrue.id,
+        };
+        arrAnswersResult.push(answerSubmit);
+    });
+
+    // if (indexRequired.length > 0) {
+    //     alert(`Bạn còn các câu ${indexRequired.slice(0, -1)} chưa làm`);
+    //     return;
+    // }
+    arrAnswersResult.forEach((item) => {
+        if (item.answerId == item.answerTrueId) {
+            console.log('đáp án đúng');
+        }
+    });
+});
